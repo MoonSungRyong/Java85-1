573d6067bf06490c6a31d8430cc3d4334dd89182
Jinyoung <jinyoung.eom@gmail.com>
2016-06-02 17:05:52 +0900

    .

--- java01/step04/Exam036.java
+++ java01/step04/Exam036.java
@@ -1,25 +1,35 @@
-//주제: 연산자 - 관계 연산자
+//주제: 연산자 - 전위 증가/감소 연산자
 
-public class Exam035 {
+public class Exam036 {
   public static void main(String[] args) {
-    //1) 문자열 + 문자열 = 문자열
-    String s1 = "홍길동";
-    String s2 = "임꺽정";
-    String s3 = s1 + s2; // 두 문자열을 연결한 새 문자열 생성
-    System.out.println(s3);
+    int a = 20;
 
-    //2) 문자열 + 숫자 = 문자열
-    String s4 = "30";
-    int i = 20;
-    System.out.println(s4 + i);
-    // 분석: 숫자가 문자열로 변경되고 두 문자열을 합쳐 새 문자열을 생성한다.
+    //1) 전위 연산자 ++
+    //=> 변수의 값을 1 증가시킨다.
+    //=> 그리고 그 변수의 값을 그 자리에 놓는다.
+    System.out.println(++a);
 
-    //3) 숫자 + 문자열 = 문자열
-    System.out.println(i + s4);
-    // 분석: i의 값을 문자열로 만든 후, 두 문자열을 합쳐 새 문자열을 생성한다.
+    //2) 전위 연산자와 할당문
+    a = 20;
+    int b = ++a;
+    System.out.printf("a=%d, b=%d\n", a, b);
+    /* 분석
+    b = ++a;
+    1) a = a + 1  => a=21
+    2) b = a
+    3) b = 21
+    */
 
-
-
+    //3) -- 전위 연산자
+    a = 20;
+    b = --a;
+    System.out.printf("a=%d, b=%d\n", a, b);
+    /* 분석
+    b = --a;
+    1) a = a - 1 => a=19
+    2) b = a
+    3) b = 19
+    */
   }
 }
 
