24790ee62a2534ab76175ea9401ecdfaf7306709
Jinyoung <jinyoung.eom@gmail.com>
2016-06-07 15:11:47 +0900

    .

--- java01/test01/Test06.java
+++ java01/test01/Test06.java
@@ -11,7 +11,15 @@ public class Test06 {
   public static void main(String[] args) {
     int inputNum = Integer.parseInt(args[0]);
     int randomNum = (int)(Math.random() * 10);
-    System.out.println(randomNum);
+    int count = 0;
+    while (inputNum > 0) {
+      if ((inputNum % 10) == randomNum)
+        count++;
+      inputNum /= 10;
+    }
+
+    System.out.printf("난수: %d\n", randomNum);
+    System.out.printf("포함 개수: %d\n", count);
   }
 }
 
