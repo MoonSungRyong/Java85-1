0c590d6ad0386c8f2aa09443451f8905018269d2
Jinyoung <jinyoung.eom@gmail.com>
2016-06-09 09:39:20 +0900

    .

---
+++ java01/test01/Test19.java
@@ -0,0 +1,24 @@
+/*문제
+1) 분모, 분자 두 개의 배열이 있다. 나눈 계산 결과 중에서 가장 큰 수를 출력하라!
+2) 배열 데이터 예:
+int[] numerator = {5, 2, 5};
+int[] denominator = {6, 3, 4};
+3) 출력 예:
+결과: 5/4
+*/
+
+
+public class Test18 {
+  public static void main(String[] args) {
+    int[] numerator = {5, 2, 5};
+    int[] denominator = {6, 3, 4};
+
+    
+  }
+
+}
+
+
+
+
+/**/
