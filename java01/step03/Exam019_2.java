eab46aa59e61ce4a758f6117b6e92472f6599e11
Jinyoung <jinyoung.eom@gmail.com>
2016-06-09 17:02:35 +0900

    .

--- java01/step03/Exam019_2.java
+++ java01/step03/Exam019_2.java
@@ -3,8 +3,8 @@
 public class Exam019_2 {
   public static void main(String[] args) {
     int v1; // 초기화 안한 상태
-    //v1++;
-    System.out.println(v1);
+    //v1++; // 초기화 안한 상태에서 값을 조회하려 하면 컴파일 오류!
+    System.out.println(v1); // 초기화 안한 상태에서 값을 조회하려 하면 컴파일 오류!
   }
 }
 
@@ -13,7 +13,8 @@ public class Exam019_2 {
 - 로컬 변수는 반드시 초기화한 후 사용해야 한다.
 - 초기화?
   값을 저장한 상태
-
+- 초기화 안한 상태에서 메모리의 값을 사용하려하면 컴파일 오류가 발생한다.
+- 단, 클래스 변수나 인스턴스 변수인 경우에는 자동 초기화된다.
 
 
 
