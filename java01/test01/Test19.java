4b27626338442c7448e4697643506302fbda3efc
Jinyoung <jinyoung.eom@gmail.com>
2016-06-09 10:23:42 +0900

    .

--- java01/test01/Test19.java
+++ java01/test01/Test19.java
@@ -8,12 +8,24 @@ int[] denominator = {6, 3, 4};
 */
 
 
-public class Test18 {
+public class Test19 {
   public static void main(String[] args) {
-    int[] numerator = {5, 2, 5};
-    int[] denominator = {6, 3, 4};
+    int[] numerator = {5, 2, 5, 2};
+    int[] denominator = {6, 3, 4, 7};
 
-    
+    // 1) n[maxIndex] / d[maxIndex] < n[i] / d[i]
+    // 2) n[maxIndex] * d[i] < n[i] * d[maxIndex]
+    // 3) maxIndex = i;
+
+    int maxIndex = 0;
+    for (int i = 1; i < numerator.length; i++) {
+      if (numerator[maxIndex] * denominator[i] <
+          numerator[i] * denominator[maxIndex] ) {
+        maxIndex = i;
+      }
+    }
+    System.out.printf("결과: %d/%d\n", numerator[maxIndex], denominator[maxIndex]);
+
   }
 
 }
---
+++ java01/test01/Test20.java
@@ -0,0 +1,27 @@
+/*문제
+1) 문자 배열이 있다. 특정 인덱스의 문자를 다른 문자로 교체하라!
+2) 배열 데이터 예:
+char[] chars = {'i', 'n', 't', 'e', 'g', 'e', 'r'};
+int n = 2;
+char ch = 'x';
+3) 실행 및 출력 예:
+>java Test20
+결과: inxeger
+*/
+
+
+public class Test20 {
+  public static void main(String[] args) {
+    char[] chars = {'i', 'n', 't', 'e', 'g', 'e', 'r'};
+    int n = 2;
+    char ch = 'x';
+
+    
+  }
+
+}
+
+
+
+
+/**/
