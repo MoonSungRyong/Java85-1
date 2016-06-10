d09ac89e984fe4f8b4c32effa5c425c712873565
Jinyoung <jinyoung.eom@gmail.com>
2016-06-10 15:32:43 +0900

    .

--- java01/test01/Array.java
+++ java01/test01/Array.java
@@ -28,6 +28,19 @@ public class Array {
       arr[i] = i;
     }
   }
+
+  static void sort(int[] arr) {
+    int temp;
+    for (int x = 1; x < arr.length; x++) {
+      for (int i = 0; i < arr.length - x; i++) {
+        if (arr[i] > arr[i+1]) {
+          temp = arr[i];
+          arr[i] = arr[i+1];
+          arr[i+1] = temp;
+        }
+      }
+    }
+  }
 }
 
 
---
+++ java01/test01/Test33.java
@@ -0,0 +1,20 @@
+/*문제
+1) 입력 값으로 문자열과 문자을 받아서,
+   문자열에 해당 문자가 몇 개있는 지 세어라!
+2) 실행 및 출력 예:
+>java Test33 computerscience c
+c: 3개
+일치하는 문자가 없습니다.
+*/
+
+
+public class Test32 {
+  public static void main(String[] args) throws Exception {
+    
+  }
+}
+
+
+
+
+/**/
