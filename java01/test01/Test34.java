e8fc3b8f3bfe9199fdb6150a336b6acaab88f6b7
Jinyoung <jinyoung.eom@gmail.com>
2016-06-10 17:05:07 +0900

    .

--- java01/test01/Test34.java
+++ java01/test01/Test34.java
@@ -17,8 +17,17 @@
 public class Test34 {
   public static void main(String[] args) throws Exception {
     String[] names = {"홍길동","임꺽정","유관순","이순신","안중근",
-                        "윤봉길","신채호","김구","김원봉","안창호"};
+                        "윤봉길","신채호","김  구","김원봉","안창호"};
     int[] scores = {95, 87, 91, 100, 56, 43, 67, 93, 82, 77};
+
+    for (int i = 0; i < names.length; i++) {
+      System.out.printf("%s: ", names[i]);
+      //별을 찍고,
+      for (int j = 0; j < scores[i] / 10; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
 
