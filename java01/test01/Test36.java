383f1d7b508379b788dff0b940de1a71041bc0b8
Jinyoung <jinyoung.eom@gmail.com>
2016-06-14 18:58:45 +0900

    .

--- java01/test01/Test36.java
+++ java01/test01/Test36.java
@@ -2,28 +2,18 @@
 */
 public class Test36 {
   public static void main(String[] args) throws Exception {
-    // 밑변의 삼각형 갯수를 입력 받아서 * 문자로 삼각형을 그려라.
-    /*  > java Test36 4
-          *
-         ***
-        *****
+    // 왼쪽 변의 길이가 8이라면,
+    /*
+    *
+    **
+    ***
+    ****
+    *****
+    ****
+    ***
+    **
+    *
     */
-    int len = Integer.parseInt(args[0]);
-    if (len % 2 == 0) {
-      len++;
-    }
-
-    int spaceLen = len / 2;
-    for (int i = 1; i <= len; i += 2) {
-      for (int j = 0; j < spaceLen; j++) {
-        System.out.print(" ");
-      }
-      spaceLen--;
-      for (int j = 0; j < i; j++) {
-        System.out.print("*");
-      }
-      System.out.println();
-    }
 
   }
 }
