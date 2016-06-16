f5c61683f17572839a593a569396f62b8d664fa7
Jinyoung <jinyoung.eom@gmail.com>
2016-06-16 17:07:31 +0900

    .

--- java01/test01/Test38_2.java
+++ java01/test01/Test38_2.java
@@ -2,15 +2,10 @@
 => 0부터 20까지 1, 2씩 순환하면서 증가한다.
 => 0 1 3 4 6 7 9 10 12 ...
 */
-public class Test38 {
+public class Test38_2 {
   public static void main(String[] args) throws Exception {
-    for (int i = 0, x = 2; i <= 20; i += x) {
+    for (int i = 0, x = 1; i <= 20; i += x, x = 3 - x) {
       System.out.println(i);
-      if (x == 2) {
-        x = 1;
-      } else {
-        x = 2;
-      }
     }
   }
 }
