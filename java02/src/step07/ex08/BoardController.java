9cea8b2e0bd21f9162dd245c0d9693143af6681a
Jinyoung <jinyoung.eom@gmail.com>
2016-06-27 15:35:42 +0900

    .

--- java02/src/step07/ex08/BoardController.java
+++ java02/src/step07/ex08/BoardController.java
@@ -33,9 +33,13 @@ public class BoardController {
         this.list();
         break;
       case "detail":
+        this.detail();
+        break;
       case "update":
+        this.update();
+        break;
       case "delete":
-        System.out.println(command);        
+        this.delete();
         break;
       default:
         System.out.println("사용할 수 없는 명령입니다.");
@@ -46,7 +50,7 @@ public class BoardController {
   void list() {
     for (int x = 0; x < this.i; x++) {
       System.out.printf("%d, %s, %s, %s, %d\n",
-        this.boards[x].no, this.boards[x].title, this.boards[x].writer,
+        x, this.boards[x].title, this.boards[x].writer,
         this.boards[x].createdDate, this.boards[x].viewCount);
     }
   }
@@ -70,8 +74,66 @@ public class BoardController {
     
     this.boards[this.i++] = board;
   }
+  
+  void detail() {
+    System.out.print("번호? ");
+    int no = Integer.parseInt(keyScanner.nextLine());
+    
+    if (no < 0 || no >= i) {
+      System.out.println("해당 게시물이 없습니다.");
+      return;
+    }
+    
+    Board board = this.boards[no];
+    System.out.printf("제목: %s\n", board.title);
+    System.out.printf("내용: %s\n", board.contents);
+    System.out.printf("작성자: %s\n", board.writer);
+    System.out.printf("등록일: %s\n", board.createdDate);
+    System.out.printf("조회수: %s\n", board.viewCount);
+  }
+  
+  void update() {
+    System.out.print("번호? ");
+    int no = Integer.parseInt(keyScanner.nextLine());
+    
+    if (no < 0 || no >= i) {
+      System.out.println("해당 게시물이 없습니다.");
+      return;
+    }
+    
+    Board board = this.boards[no];
+    System.out.printf("제목(%s)? ", board.title);
+    board.title = keyScanner.nextLine();
+    System.out.printf("내용(%s)? ", board.contents);
+    board.contents = keyScanner.nextLine();
+  }
+  
+  void delete() {
+    System.out.print("번호? ");
+    int no = Integer.parseInt(keyScanner.nextLine());
+    
+    if (no < 0 || no >= i) {
+      System.out.println("해당 게시물이 없습니다.");
+      return;
+    }
+    
+    for (int x = no; x < this.i; x++) {
+      this.boards[x] = this.boards[x + 1]; 
+    }
+    this.i--;
+  }
+  
 }
 
 /*
 
 */
+
+
+
+
+
+
+
+
+
