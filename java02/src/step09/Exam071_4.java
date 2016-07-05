97825c6972552a7e0482f5521c6f6aac1731d612
Jinyoung <jinyoung.eom@gmail.com>
2016-07-05 14:30:50 +0900

    .

--- java02/src/step09/Exam071_4.java
+++ java02/src/step09/Exam071_4.java
@@ -18,7 +18,22 @@ public class Exam071_4 {
     names.add(new Integer(20)); // 참나.. 별놈의 것도 다 저장하는 군.
     names.add(true);  //auto-boxing이 수행된다. names.add(new Boolean(true));와 같다. 
     
+    // 문제점: 특정 타입의 객체(의 주소)만 저장하고 싶은데 그것을 막을 방법이 없다.
+    // 해결책: 어떤 타입의 객체를 저장할 지 지정하라!
     
+    //2) 제네릭 사용 후
+    //   => ArrayList에 담을 데이터의 타입을 제한한다.
+    //ArrayList<String> names2 = new ArrayList<String>();
+    ArrayList<String> names2 = new ArrayList<>(); // new 명령 다음에 타입 지정을 생략할 수 있다.
+    names2.add("홍길동"); // 상수 스트링 객체의 주소를 저장
+    names2.add(new String("임꺽정")); // 그냥 일반 스트링 객체의 주소를 저장
+    //names2.add(new Integer(2)); // 컴파일 오류! 지정된 타입이 아니면 저장 불가!
+    //names2.add(new Date()); // 컴파일 오류!
+    
+    // 제네릭 사용 이점:
+    // => 지정된 타입 외에 다른 타입의 객체를 저장할 수 없다. 
+    // => 그것도 실행 전 컴파일 단계에서 검사한다. => 오류를 쉽게 잡을 수 있다. 
+    // => 개발자의 의도에 반하여 동작하는 것을 최소화 할 수 있다. => 프로그램 안정성이 좋아진다.
   }
 
 }
