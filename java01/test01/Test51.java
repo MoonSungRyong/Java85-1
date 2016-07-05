7d7196c48680bbfdfb4cf4ab7120ad83bf584be4
Jinyoung <jinyoung.eom@gmail.com>
2016-07-05 10:09:10 +0900

    .

--- java01/test01/Test51.java
+++ java01/test01/Test51.java
@@ -1,47 +1,34 @@
 /* 문제:
- * => 2단에서 18단까지 구구단을 출력한다.
- * => 2, 3, 5, 8, 12, ... 식으로 단을 증가시키면서 구구단을 출력한다.
- * => 단이 짝수이면 짝수 곱만 출력하고, 단이 홀수이면 홀수 곱만 출력한다.
+ * => 사용자로부터 숫자를 입력 받아 0부터 9까지 숫자가 등장하는 개수를 출력하라
  * 예)
- * 2 * 2 = 4
- * 2 * 4 = 8
- * 2 * 6 = 12
- * 2 * 8 = 16
- * 3 * 1 = 3
- * 3 * 3 = 9
- * 3 * 5 = 15
- * 3 * 7 = 21
- * 3 * 9 = 27
- * 5 * 1 = 5
- * ...
+243300985
+0: 2
+2: 1
+3: 2
+4: 1
+5: 1
+8: 1
+9: 1
 */
 import java.util.Scanner;
 
 public class Test51 {
   public static void main(String[] args) throws Exception {
-    // 사용자로부터 밑변의 길이를 입력받아 삼각형을 출력하시오
-    //   *
-    //  ***
-    // *****
     Scanner keyScan = new Scanner(System.in);
-    int len = Integer.parseInt(keyScan.nextLine());
+    int value = Integer.parseInt(keyScan.nextLine());
     
-    int i = 1, j, spaceLen = len / 2;
-    while (i <= len + 1) {
-      j = 0;
-      while (j < spaceLen) {
-        System.out.print(" ");
-        j++;
-      }
-      spaceLen--;
-      
-      j = 0;
-      while (j < i) {
-        System.out.print("*");
-        j++;
-      }
-      System.out.println();
-      i += 2;
+    int[] numCount = new int[10];
+    for (int i = 0; i < numCount.length; i++) 
+      numCount[i] = 0;
+    
+    while (value > 0) {
+      numCount[value % 10]++;
+      value /= 10;
+    }
+    
+    for (int i = 0; i < numCount.length; i++) {
+      if (numCount[i] > 0)
+        System.out.printf("%d: %d\n", i, numCount[i]);
     }
   }
   
