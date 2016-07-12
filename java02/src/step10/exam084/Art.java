39bc20eb299090848bfb25ae061d3a9ac06d2824
Jinyoung <jinyoung.eom@gmail.com>
2016-07-12 11:52:10 +0900

    .

---
+++ java02/src/step10/exam084/Art.java
@@ -0,0 +1,7 @@
+package step10.exam084;
+
+public class Art {
+  static final int PHOTO = 31;
+  static final int PICTURE = 32;
+  static final int SCULPTURE = 33;
+}
---
+++ java02/src/step10/exam084/Language.java
@@ -0,0 +1,8 @@
+package step10.exam084;
+
+public class Language {
+  static final int ENGLISH = 21;
+  static final int JAPANESE = 22;
+  static final int CHINESE = 23;
+  static final int SPANISH = 24;
+}
