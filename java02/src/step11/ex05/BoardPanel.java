0bfb5c7a4078f2d21ee176a88c0048590699067c
Jinyoung <jinyoung.eom@gmail.com>
2016-07-13 12:56:50 +0900

    .

---
+++ java02/src/step11/ex05/BoardPanel.java
@@ -0,0 +1,75 @@
+package step11.ex05;
+
+import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
+
+public class BoardPanel extends Panel {
+  public BoardPanel() {
+    setLayout(new FlowLayout(FlowLayout.LEFT));
+    
+    Label label = new Label("게시판 화면");
+    label.setPreferredSize(new Dimension(400, 30));
+    add(label);
+
+    label = new Label("게시물 목록");
+    label.setPreferredSize(new Dimension(400, 30));
+    add(label);
+    
+    TextArea ta = new TextArea();
+    ta.setPreferredSize(new Dimension(400, 200));
+    add(ta);
+    
+    label = new Label("게시물 입력/상세정보");
+    label.setPreferredSize(new Dimension(400, 30));
+    add(label);
+    
+    Panel rowPanel = new Panel(new FlowLayout(FlowLayout.LEFT));
+    label = new Label("제목");
+    label.setPreferredSize(new Dimension(40, 30));
+    rowPanel.add(label);
+    
+    TextField titleTF = new TextField();
+    titleTF.setPreferredSize(new Dimension(350, 30));
+    rowPanel.add(titleTF);
+    add(rowPanel);
+    
+    rowPanel = new Panel(new FlowLayout(FlowLayout.LEFT));
+    label = new Label("내용");
+    label.setPreferredSize(new Dimension(40, 30));
+    rowPanel.add(label);
+    
+    TextArea contentTA = new TextArea();
+    contentTA.setPreferredSize(new Dimension(350, 80));
+    rowPanel.add(contentTA);
+    add(rowPanel);
+    
+    rowPanel = new Panel(new FlowLayout(FlowLayout.LEFT));
+    label = new Label("암호");
+    label.setPreferredSize(new Dimension(40, 30));
+    rowPanel.add(label);
+    
+    TextField passwordTF = new TextField();
+    passwordTF.setPreferredSize(new Dimension(350, 30));
+    passwordTF.setEchoChar('*');
+    rowPanel.add(passwordTF);
+    add(rowPanel);
+    
+    rowPanel = new Panel(new FlowLayout(FlowLayout.LEFT));
+    Button addBtn = new Button("등록");
+    rowPanel.add(addBtn);
+    Button cancelBtn = new Button("취소");
+    rowPanel.add(cancelBtn);
+    add(rowPanel);
+    
+  }
+}
+
+
+
+
+
---
+++ java02/src/step11/ex05/MemberPanel.java
@@ -0,0 +1,11 @@
+package step11.ex05;
+
+import java.awt.Label;
+import java.awt.Panel;
+
+public class MemberPanel extends Panel {
+  public MemberPanel() {
+    Label label = new Label("회원관리 화면");
+    add(label);
+  }
+}
