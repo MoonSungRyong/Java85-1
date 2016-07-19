aed5e3995b3378d22107e04343b0ea3527a617cf
Jinyoung <jinyoung.eom@gmail.com>
2016-07-19 11:23:49 +0900

    .

--- java02/src/designpatterns/decorator/Test01.java
+++ java02/src/designpatterns/decorator/Test01.java
@@ -3,8 +3,24 @@ package designpatterns.decorator;
 public class Test01 {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    //1) 데코레이터 붙이기 전
+    WorkerRobot w1 = new WorkerRobot();
+    w1.run();
+    System.out.println("---------------");
+    
+    //2) SwordDecorator 붙이기
+    SwordDecorator d1 = new SwordDecorator(w1);
+    d1.run();
+    System.out.println("---------------");
+    
+    //3) GunDecorator 붙이기
+    GunDecorator d2 = new GunDecorator(w1);
+    d2.run();
+    System.out.println("---------------");
+    
+    //4) SwordDecorator에다가 ThrowDecorator를 덧 붙인다.
+    ThrowDecorator d3 = new ThrowDecorator(d2);
+    d3.run();
   }
 
 }
