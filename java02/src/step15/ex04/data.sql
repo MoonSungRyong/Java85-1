971dc317e058f618f6fc98e048af3994abe97c8f
Jinyoung <jinyoung.eom@gmail.com>
2016-08-09 09:32:37 +0900

    .

---
+++ java02/src/step15/ex04/data.sql
@@ -0,0 +1,9 @@
+/* 학습모듈 데이터 입력 */
+insert into BIT_SUBJ_MODS(TITL) VALUES('SW아키텍처');
+insert into BIT_SUBJ_MODS(TITL) VALUES('응용SW엔지니어링');
+insert into BIT_SUBJ_MODS(TITL) VALUES('임베디드SW엔지니어링');
+insert into BIT_SUBJ_MODS(TITL) VALUES('DB엔지니어링');
+insert into BIT_SUBJ_MODS(TITL) VALUES('NW엔지니어링');
+insert into BIT_SUBJ_MODS(TITL) VALUES('보안엔지니어링');
+insert into BIT_SUBJ_MODS(TITL) VALUES('UI/UX엔지니어링');
+insert into BIT_SUBJ_MODS(TITL) VALUES('시스템SW엔지니어링');
