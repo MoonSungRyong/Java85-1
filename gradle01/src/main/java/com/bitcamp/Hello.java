30bf72a5854c32fa60784ac456f4a21121cd1d2c
Jinyoung <jinyoung.eom@gmail.com>
2016-08-12 12:16:51 +0900

    .

--- gradle01/src/main/java/com/bitcamp/Hello.java
+++ gradle01/src/main/java/com/bitcamp/Hello.java
@@ -1,8 +1,19 @@
 package com.bitcamp;
 
+import java.io.FileInputStream;
+
 /* 한글 주석...*/
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+    
+    try (
+      FileInputStream in = new FileInputStream("build.gradle");
+    ) {
+      System.out.println("테스트...");
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
