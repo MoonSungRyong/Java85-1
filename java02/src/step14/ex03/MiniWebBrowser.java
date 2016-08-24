4e0dc3cc50237d56d4a526fdf6df86a197f03f78
Jinyoung <jinyoung.eom@gmail.com>
2016-08-24 12:43:44 +0900

    .

--- java02/src/step14/ex03/MiniWebBrowser.java
+++ java02/src/step14/ex03/MiniWebBrowser.java
@@ -8,14 +8,14 @@ import java.net.Socket;
 public class MiniWebBrowser {
 
   public static void main(String[] args) throws Exception {
-    Socket socket = new Socket("finance.naver.com", 80);
+    Socket socket = new Socket("localhost", 8080);
     
     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     PrintStream out = new PrintStream(socket.getOutputStream());
     
     //웹서버에 요청하기 => HTTP 프로토콜 형식에 따라서 요청해야 한다.
-    out.println("GET / HTTP/1.1");
-    out.println("Host: finance.naver.com");
+    out.println("HEAD /web01/servlet14 HTTP/1.1");
+    out.println("Host: localhost:8080");
     out.println();
     
     //웹서버로부터 응답을 읽기 => HTTP 프로토콜 형식에 따라서 응답 내용을 읽어야 한다.
