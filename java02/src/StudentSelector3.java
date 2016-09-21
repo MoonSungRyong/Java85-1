e5d62cd4a6b09af9b5167266e0cfc48233edc921
Jinyoung <jinyoung.eom@gmail.com>
2016-09-21 16:17:31 +0900

    .

--- java02/src/StudentSelector3.java
+++ java02/src/StudentSelector3.java
@@ -3,19 +3,26 @@ import java.util.ArrayList;
 public class StudentSelector3 {
   public static void main(String[] args) throws Exception {
     String[] students = {
-      "e에2승", "문선애팀"
+      "e에2승", "문선애팀", "원지선과난장이들", "류세현과외동들", "백권호팀"
     };
 
+    ArrayList<String> list = new ArrayList<>();
+    for (String name : students) {
+      list.add(name);
+    }
+    
     int no = 0;
     System.out.println("선택된 팀은?");
     
-    for (int i = 0; i < 77; i++) {
-      System.out.print(".");
-      Thread.currentThread().sleep(200);
-      no = (int)(Math.random() * 2);
+    while (list.size() > 0) {
+      for (int i = 0; i < 20; i++) {
+        System.out.print(".");
+        Thread.currentThread().sleep(200);
+        no = (int)(Math.random() * list.size());
+      }
+      System.out.println(list.remove(no));
     }
     
-    System.out.println(students[no]);
   }
 }
 
